b93ed13c36ed232f5c17d1fe16f5eae0b89099d4
kenny robert <skenrobert@gmail.com>
2023-02-26 16:54:53 -0500

    change db last time

--- myapp/settings.py
+++ myapp/settings.py
@@ -80,7 +80,10 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-'default': dj_database_url.config(default='postgresql://postgres:postgres@localhost:5432/mysite',        conn_max_age=600    )}
+'default':dj_database_url.config(
+        default='postgresql://postgres:postgres@localhost/postgres',
+        conn_max_age=600    
+        )}
 
 
 
